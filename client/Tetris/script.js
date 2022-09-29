4fff36e55f974e2b4a4b9ab252ac5de4e04e62ad
gdemattia <gdematti@terpmail.umd.edu>
2022-09-29 19:14:04 -0400

    added functions to JS

--- client/Tetris/script.js
+++ client/Tetris/script.js
@@ -4,5 +4,117 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDispaly = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
-  console.log(squares);
+  
+  // Shapes
+  const lTeromino = [
+    [1, width+1, width * 2 + 1, 2],
+    [width, width + 1, width+2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 +2]
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+
+  const theTetrominos = [lTeromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+  // randomly select shape
+  let random = Math.floor(Math.random() * theTetrominos.length);
+  let current = theTetrominos[random][currentRotation];
+
+  // draw first shape
+
+  function draw () {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
+  draw();
+
+  // Undraw shape
+
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    });
+  }
+
+  // Make shape move
+
+  timerId = setInterval(moveDown, 1000);
+
+  // assign controls
+
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft();
+    }
+  }
+
+  // Move down function
+
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  // Freeze function
+
+  function freeze() {
+    if(current.some(index => squares[currentPosition + index +width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+
+      // Start new shape falling
+
+      random = Math.floor(Math.random() * theTetrominos.length);
+      current = theTetrominos[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
+
+  // Move shape left unless at end of grid
+
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+
+    if (!isAtLeftEdge) currentPosition -= 1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1;
+    }
+
+    draw();
+  }
 });
